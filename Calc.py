6d901f0f8a4193bb05c9ac8137dbc6ffb5ae39d4
Israelnjor <ibkiche@st.ug.edu.gh>
2026-01-29 22:24:50 +0000

    Implement basic calculator functionality
    Fix input handling and calculation logic in calculator function.

---
+++ Calc.py
@@ -0,0 +1,42 @@
+def calc(a, b):
+  total = a + b
+  product = a * b
+  return total, product
+s, p = calc(56, 34)
+print(s, p)
+
+def calculator():
+    x = input("Enter your first number: ")
+    y = input("Enter your second number: ")
+    add = x + y
+    prod = x * Y
+    diff = x - y
+    div = x / y
+    return add, prod, diff, div
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+
+
+
+
+
+
+
